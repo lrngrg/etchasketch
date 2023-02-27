ea751550fded1cd9a448638cc1cb10dc80a40ffb
larni <lauren.gerig@gmail.com>
2023-02-27 18:20:37 -0500

    Write succesful makeGrid for loop function
    makeGrid is a nested forLoop.  I could maybe make it a bit more concise by keeping the functions
    outside of the main function and pass them into the function but hey, it works so far

--- scripts.js
+++ scripts.js
@@ -1,23 +1,26 @@
 //assign container div
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 
 
-//create function to make a grid
-//take a number
-//loop through to create that number of rows
-//for each item in the row, want to add a cell in the y axis for same number given
 
-//so far this is just creating the correct number of divs
+
 
 function makeGrid(num) {
+
     for (let i = 0; i < num; i++) {
+        const column = document.createElement('div');
+        container.appendChild(column);
+        column.classList.add('column');
+        console.log('i'+i);
+
         for (let j = 0; j < num; j++) {
             const cell = document.createElement('div');
-            container.appendChild(cell);
-            cell.classList.add('cell')
-            console.log('x')
+            column.appendChild(cell);
+            cell.classList.add('cell');
+            console.log('j'+j);
         }
     }
 }
 
 makeGrid(16);
+
