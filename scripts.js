f2475e91608ea353c5a888796781823bc2d91a51
larni <lauren.gerig@gmail.com>
2023-02-27 19:24:39 -0500

    Add mouseover event to grid

--- scripts.js
+++ scripts.js
@@ -1,26 +1,24 @@
-//assign container div
 const container = document.querySelector('.container');
 
-
-
-
-
 function makeGrid(num) {
-
     for (let i = 0; i < num; i++) {
         const column = document.createElement('div');
         container.appendChild(column);
         column.classList.add('column');
-        console.log('i'+i);
-
         for (let j = 0; j < num; j++) {
             const cell = document.createElement('div');
             column.appendChild(cell);
             cell.classList.add('cell');
-            console.log('j'+j);
         }
     }
 }
 
 makeGrid(16);
 
+const cellElement = document.querySelectorAll('.cell');
+
+cellElement.forEach((div) => {
+    div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'black';
+    })
+});
